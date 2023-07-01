8a37e06e3ea30606295ed429ff1b620bfb85b103
Thomas Hutchinson <thomashutchinson96@gmail.com>
2023-07-01 18:49:08 +0100

    Updated Navigation and Fixed Routing

--- resources/views/employees/list.blade.php
+++ resources/views/employees/list.blade.php
@@ -2,5 +2,23 @@
 @section('title', 'Employee List')
 
 @section('body')
-    <p>Employee List</p>
+    <h5>Employee List</h5>
+    <table>
+        <thead>
+            <tr>
+                <td>Name</td><td>Department</td><td>Manager?</td><td>Edit</td><td>Delete</td>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($employees as $employee)
+                <tr>
+                    <td>{{ "$employee->firstname $employee->surname" }}</td>
+                    <td>{{ $employee->department?->name ?? "No Department" }}</td>
+                    <td>{{ $employee->user?->manager ? "Yes" : "No" }}</td>
+                    <td><a href="./employee/edit/{{$employee->id}}">Edit</a></td>
+                    <td><a href="./employee/delete/{{$employee->id}}">Delete</a></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
